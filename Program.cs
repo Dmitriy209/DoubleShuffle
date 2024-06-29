8f7689f9306b64a5779bb9079679c849da96accd
Dmitriy209 <dim286754@gmail.com>
2024-06-29 12:40:30 +0500

    С обобщениями

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace DoubleShuffle
             const string ButtonExit = "exit";
             const string ButtonShuffleCards = "1";
             const string ButtonShuffleNumbers = "2";
+            const string ButtonShuffleAll = "3";
 
             bool isRunning = true;
 
@@ -20,7 +21,8 @@ namespace DoubleShuffle
             {
                 Console.WriteLine($"Введите {ButtonExit}, чтобы выйти.\n" +
                     $"Введите {ButtonShuffleCards}, чтобы перемешать колоду.\n" +
-                    $"Введите {ButtonShuffleNumbers}, чтобы перемешать числа.");
+                    $"Введите {ButtonShuffleNumbers}, чтобы перемешать числа.\n" +
+                    $"Введите {ButtonShuffleAll}, чтобы перемешать и колоду и числа.");
                 string userInput = Console.ReadLine();
 
                 Console.Clear();
@@ -37,6 +39,11 @@ namespace DoubleShuffle
 
                     case ButtonShuffleNumbers:
                         ShuffleNumbers(numbers);
+                        break;
+
+                    case ButtonShuffleAll:
+                        ShuffleAll(cards);
+                        ShuffleAll(numbers);
                         break;
 
                     default:
@@ -138,6 +145,53 @@ namespace DoubleShuffle
         static void ShowArrayInteger(int[] array, string separator = "")
         {
             foreach (int element in array)
+                Console.Write(element + separator);
+
+            Console.WriteLine();
+        }
+
+        static T[] ShuffleAnything<T>(T[] array)
+        {
+            Random random = new Random();
+
+            int minLimitRandom = 0;
+            int maxLimitRandom = array.Length;
+
+            T tempElement;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int indexRandom = random.Next(minLimitRandom, maxLimitRandom);
+
+                tempElement = array[i];
+                array[i] = array[indexRandom];
+                array[indexRandom] = tempElement;
+            }
+
+            return array;
+        }
+
+        static void ShuffleAll<T>(T[] array)
+        {
+            string space = " ";
+
+            T[] numbersMix = new T[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+                numbersMix[i] = array[i];
+
+            Console.WriteLine($"Изначальный массив.");
+            ShowArrayAll(array, space);
+
+            numbersMix = ShuffleAnything(numbersMix);
+
+            Console.WriteLine("Перемешанный массив.");
+            ShowArrayAll(numbersMix, space);
+        }
+
+        static void ShowArrayAll<T>(T[] array, string separator = "")
+        {
+            foreach (T element in array)
                 Console.Write(element + separator);
 
             Console.WriteLine();
