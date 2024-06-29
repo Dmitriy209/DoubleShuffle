09f9187b007680d66fef51024396ed3c069f60c9
Dmitriy209 <dim286754@gmail.com>
2024-06-29 11:58:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace DoubleShuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ButtonExit = "exit";
+            const string ButtonShuffleCards = "1";
+            const string ButtonShuffleNumbers = "2";
+
+            bool isRunning = true;
+
+            string[] cards = { "J", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "Q", "K", "A" };
+
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            while (isRunning)
+            {
+                Console.WriteLine($"Введите {ButtonExit}, чтобы выйти.\n" +
+                    $"Введите {ButtonShuffleCards}, чтобы перемешать колоду.\n" +
+                    $"Введите {ButtonShuffleNumbers}, чтобы перемешать числа.");
+                string userInput = Console.ReadLine();
+
+                Console.Clear();
+
+                switch (userInput)
+                {
+                    case ButtonExit:
+                        isRunning = false;
+                        break;
+
+                    case ButtonShuffleCards:
+                        ShuffleCards(cards);
+                        break;
+
+                    case ButtonShuffleNumbers:
+                        ShuffleNumbers(numbers);
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели что-то другое.");
+                        break;
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Вы вышли из программы.");
+        }
+
+        static string[] Shuffle(string[] symbols)
+        {
+            Random random = new Random();
+
+            int minLimitRandom = 0;
+            int maxLimitRandom = symbols.Length;
+
+            string tempElement;
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                int indexRandom = random.Next(minLimitRandom, maxLimitRandom);
+
+                tempElement = symbols[i];
+                symbols[i] = symbols[indexRandom];
+                symbols[indexRandom] = tempElement;
+            }
+
+            return symbols;
+        }
+
+        static void ShuffleCards(string[] cards)
+        {
+            string space = " ";
+
+            string[] cardsMix = new string[cards.Length];
+
+            for (int i = 0; i < cards.Length; i++)
+                cardsMix[i] = cards[i];
+
+            Console.WriteLine($"Изначальная колода:");
+            ShowArray(cards, space);
+
+            cardsMix = Shuffle(cardsMix);
+
+            Console.WriteLine("Ваша колода, сэр.");
+            ShowArray(cardsMix, space);
+        }
+
+        static void ShowArray(string[] array, string separator = "")
+        {
+            foreach (string element in array)
+                Console.Write(element + separator);
+
+            Console.WriteLine();
+        }
+
+        static int[] ShuffleInteger(int[] numbers)
+        {
+            Random random = new Random();
+
+            int minLimitRandom = 0;
+            int maxLimitRandom = numbers.Length;
+
+            int tempElement;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int indexRandom = random.Next(minLimitRandom, maxLimitRandom);
+
+                tempElement = numbers[i];
+                numbers[i] = numbers[indexRandom];
+                numbers[indexRandom] = tempElement;
+            }
+
+            return numbers;
+        }
+
+        static void ShuffleNumbers(int[] numbers)
+        {
+            string space = " ";
+
+            int[] numbersMix = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbersMix[i] = numbers[i];
+
+            Console.WriteLine($"Изначальная колода:");
+            ShowArrayInteger(numbers, space);
+
+            numbersMix = ShuffleInteger(numbersMix);
+
+            Console.WriteLine("Ваша колода, сэр.");
+            ShowArrayInteger(numbersMix, space);
+        }
+
+        static void ShowArrayInteger(int[] array, string separator = "")
+        {
+            foreach (int element in array)
+                Console.Write(element + separator);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DoubleShuffle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoubleShuffle")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("de75ae91-a017-4d5f-94a4-13e3de896edb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
